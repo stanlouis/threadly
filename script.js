e0711d34e110f6103d377d194a2e0fc3a1255665
Stanley Louis <stanlouis@msn.com>
2018-05-19 22:19:57 -0400

    Save and retrieve posts from and to localStorate

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 $ (function () {
+  function loadPosts () {
+    let tasks = JSON.parse (localStorage.getItem ('comments'));
+    if (tasks !== null) {
+      tasks.forEach (comment => {
+        let item = $ ('<li>').text (comment);
+        item.prependTo ('#comments');
+      });
+    }
+  }
+  loadPosts ();
   $ ('form').submit (e => {
     let $input = $ (e.target).find ('input');
     let comment = $input.val ();
+    $input.val ('');
+    
+    let comments;
 
-    if (comment !== '') {
-      let html = $ ('<li>').text (comment);
-      html.prependTo ('#comments');
-      $input.val ('');
+    if (localStorage.getItem ('comments') === null) {
+      // if local storage is empty set comments to empty array
+      comments = [];
+    } else {
+      comments = JSON.parse (localStorage.getItem ('comments'));
     }
-    return false;
+
+    comments.push (comment);
+    // Store the comments into localStorage using "localStorage.setItem"
+    localStorage.setItem ('comments', JSON.stringify (comments));
+    //remove all li(s) to prevent duplicate
+    $ ('#comments').empty ();
+    loadPosts ();
+    e.preventDefault ();
   });
 });
